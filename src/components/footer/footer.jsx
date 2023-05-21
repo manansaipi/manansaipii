8163701babae838cc6f9b441a960dee70d2355a5
manansaipi <abdul.saipi@student.president.ac.id>
2023-05-21 11:04:34 +0700

    updt

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,39 +1,58 @@
 import React from 'react'
 import './footer'
-
+import {BiMessageRoundedDots} from 'react-icons/bi'
 const footer = () => {
   return (
      <div style={styles.content}>
-        <footer style={styles.footer}>
-   <div style={styles.space}></div>
+    <footer style={styles.footer}>
+      <div style={styles.space}></div>
       <div style={styles.container}>
-        <p style={styles.text}> @manansaipi</p>
+        <p style={styles.textLeft}>Hidden Left Text</p>
+        <p style={styles.textCenter}>@manansaipi</p>
+        <a href="https://ngl.link/manansaipi" style={{ textDecoration: 'none', color: 'inherit' }} target="_blank" rel="noreferrer"><BiMessageRoundedDots style={styles.icon}/></a>
+      
       </div>
     </footer>
-      </div>
+  </div>
    
   )
 }
 const styles = {
-   content: {
-      marginTop: '200px', 
-    },
+  content: {
+    marginTop: '200px',
+  },
   footer: {
     backgroundColor: '#f2f2f2',
     padding: '20px',
     textAlign: 'center',
   },
   container: {
-    maxWidth: '960px',
-    margin: '0 auto',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    overflowX: 'auto',
   },
-  text: {
+  textLeft: {
+    fontSize: '0px',
+    color: '#f2f2f2',
+    visibility: 'hidden',
+    margin: 0,
+  },
+  textCenter: {
     fontSize: '14px',
     color: '#888',
+    margin: 0,
   },
-   space: {
-    height: '20px',
+  textRight: {
+    fontSize: '14px',
+    color: '#888',
+    margin: 0,
   },
+  icon: {
+    fontSize: '20px',
+    color:"#6E6C78",
+  },
+
 };
 
 export default footer
